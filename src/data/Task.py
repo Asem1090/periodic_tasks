91f0ae93d9a9893de4d31772ad56c0e645af25d4
Asem_O <asem.sh2004@gmail.com>
2023-09-03 18:36:58 +0300

    Completed Task.py (for now)

--- src/data/Task.py
+++ src/data/Task.py
@@ -6,10 +6,13 @@ from datetime import datetime
 class Task:
     DATETIME_FORMAT = "%d/%m/%Y %H:%M"
 
-    def __init__(self, task_name: str, task_repetition: int, last_completion_date):
-        self.task_name = task_name
-        self.task_repetition = int(task_repetition)
+    def __init__(self, name: str, task_repetition: int, last_completion_date: datetime = None):
+        self.name = name
+        self.repetition = int(task_repetition)
         self.last_completion_date = last_completion_date
+
+    def __str__(self):
+        return f"{self.name:<25}{self.repetition:<5}{self.last_completion_date:<20}"
 
     @staticmethod
     def from_csv(csv_line: str) -> Task:
@@ -21,4 +24,4 @@ class Task:
         if last_completion_date is None:
             last_completion_date = datetime.now()
 
-        return f"{self.task_name},{self.task_repetition},{last_completion_date.strftime(Task.DATETIME_FORMAT)}"
+        return f"{self.name},{self.repetition},{last_completion_date.strftime(Task.DATETIME_FORMAT)}"
